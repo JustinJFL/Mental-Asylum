a58533063e14daaf966bc23f7f7cd9ea744397f0
nknights <nlotruglio@knights.ucf.edu>
2020-01-19 14:40:14 -0500

    UI and Start Menu ready for prototype

---
+++ Mental Asylum/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("MentalAsylumPOC1");
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT DAMN YOU!");
+        Application.Quit();
+    }
+}
