bb37f872f8395c960b1c9504d52ae3dad65429d7
JustinJFL <52304712+JustinJFL@users.noreply.github.com>
2020-01-19 15:42:35 -0500

    Enemy Controller built
    completed the enemy controller and spawner, they spawn when the player enters a specific area. still working on

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float moveSpeed = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //makes enemy walk directly towards player current position
+        transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform);
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        Destroy(gameObject,Random.Range(7.0f,12.0f));
+    }
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject enemySpawn;
+    public GameObject enemy;
+    public Transform enemySpawnPos;
+
+    public float repeatRate;
+
+    private bool spawnStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnStatus = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("entered the spawn area");
+        //uses invokerepeating to repeadedly spawn enemy at every "repeatRate" seconds
+        if(other.gameObject.tag =="Player" && spawnStatus == true)
+        {
+        InvokeRepeating("SpawnEnemy",.5f, repeatRate);
+        //gameObject.GetComponent<BoxCollider>().enabled = false;
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        spawnStatus = false;
+        Debug.Log("left the spawn area");
+    }
+    void SpawnEnemy()
+    {
+        Instantiate(enemy,enemySpawnPos.position, enemySpawnPos.rotation);
+    }
+}
+
